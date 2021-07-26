07ea43496147633c114d1be83e1a1bcf82be225b
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2021-07-26 19:36:12 -0300

    Creating an putting on factories and seeders

---
+++ marketplace/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        DB::table('users') -> insert(
+            [
+                'name' => 'Test',
+                'email' => 'test_seed@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => 'test_test_test',    
+            ]
+        );
+
+        /*
+        If you want to create more than one user per db:seed
+        factory(\App\User::class, number of users you want) -> create();        
+        */
+    }
+}
