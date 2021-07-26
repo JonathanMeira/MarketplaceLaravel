cc16230e779cf9e39f4a67d7172bff3dbf3d27c4
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2021-07-26 20:25:06 -0300

    Creating models and testing routes

--- marketplace/routes/web.php
+++ marketplace/routes/web.php
@@ -14,3 +14,47 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/models',function(){
+    
+/*
+    Using Active Records:
+
+    $user = new \App\User();
+    $user -> name = 'UserTest';
+    $user -> email ='userTestEmail@gmail.com';
+    $user -> password = bcrypt('12345678');
+
+    $user -> save();
+
+    return \App\User::all();
+*/
+
+/*
+    Using Mass Assignment:
+
+    $user = \App\User::create([
+            'name' => 'UserTest2',
+            'email' => 'userTest2Email@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+    dd($user);
+*/
+   
+/*
+    Using Mass Update:
+
+    $user = \App\User::find(1); 
+    $user -> update([
+             'name' => '*New Name*'
+        ]);
+
+    dd($user);
+
+*/
+
+    return \App\User::all();
+
+
+});
