e325c970e1555ac932017ada74059e1338a39782
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2021-07-26 22:02:16 -0300

    Creating seeders for stores and categories

--- marketplace/database/seeds/UsersTableSeeder.php
+++ marketplace/database/seeds/UsersTableSeeder.php
@@ -13,8 +13,8 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         
-
-        DB::table('users') -> insert(
+        /* 
+            DB::table('users') -> insert(
             [
                 'name' => 'Test',
                 'email' => 'test_seed@gmail.com',
@@ -22,11 +22,18 @@ class UsersTableSeeder extends Seeder
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => 'test_test_test',    
             ]
-        );
+        );    
+        */
 
         /*
         If you want to create more than one user per db:seed
         factory(\App\User::class, number of users you want) -> create();        
         */
+
+        factory(\App\User::class, 5) -> create()-> each(function($user){
+            $user->store()->save(factory(\App\Store::class)->make());
+        });
+
+
     }
 }
