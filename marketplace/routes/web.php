2655ad4580c15161a67b947506dcb90e7f15bc40
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2021-07-28 22:48:36 -0300

    Adding header to project and tweaking routes

--- marketplace/routes/web.php
+++ marketplace/routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/models',function(){
     
@@ -136,4 +136,4 @@ Route::group(['middleware' => ['auth']], function(){
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
