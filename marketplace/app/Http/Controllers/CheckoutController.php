db4a031e2f6b1c0fdbd18bb2fb045573305ad6c7
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2021-08-09 18:07:16 -0300

    Processing user's credit card information

--- marketplace/app/Http/Controllers/CheckoutController.php
+++ marketplace/app/Http/Controllers/CheckoutController.php
@@ -12,15 +12,135 @@ class CheckoutController extends Controller
             return redirect()->route('login');
         }
         
-        $this -> makePagSeguroSession();
         //session()->forget('pagseguro_session_code');
+        
+        $this -> makePagSeguroSession();
+        
 
-        return view('checkout');
+        $cartItems = array_map(function($line){
+            return $line['amount'] * $line['price'];
+        }, session()->get('cart'));
+
+        $cartItems = array_sum($cartItems);
+
+        return view('checkout', compact('cartItems'));
     }
 
     public function proccess(Request $request)      
     {
+
+    $dataPOST = $request -> all();
+    $reference = 'XPTO';
+
+    $creditCard = new \PagSeguro\Domains\Requests\DirectPayment\CreditCard();
+
+    $creditCard->setReceiverEmail(env('PAGSEGURO_EMAIL'));
+
+    $creditCard->setReference($reference);
+
+    // Set the currency
+    $creditCard->setCurrency("BRL");
+
+    // Add items for this payment request
+
+    $cartItems = session()->get('cart');
+
+    foreach($cartItems as  $item){
+        $creditCard->addItems()->withParameters(
+            $reference,
+            $item['name'],
+            $item['amount'],
+            $item['price']
+        );
+    }
+
+    // Set your customer information.
+    // If you using SANDBOX you must use an email @sandbox.pagseguro.com.br
+
+    $user = auth()->user();
+    $email = env('PAGSEGURO_ENV') == 'sandbox' ? 'c53281149833065927639@sandbox.pagseguro.com.br' : $user->email;
+
+    $creditCard->setSender()->setName($user->name);
+    $creditCard->setSender()->setEmail($email);
+
+    //This is an example that can be changed to future implementations
+    $creditCard->setSender()->setPhone()->withParameters(
+        11,
+        56273440
+    );
+
+    //This is an example that can be changed to future implementations
+    $creditCard->setSender()->setDocument()->withParameters(
+        'CPF',
+        '38883880749'
+    );
+
+    $creditCard->setSender()->setHash($dataPOST['hash']);
+
+    //This is an example that can be changed to future implementations
+    $creditCard->setSender()->setIp('127.0.0.0');
+
+    // Set shipping information for this payment request
+    //This is an example that can be changed to future implementations
+    $creditCard->setShipping()->setAddress()->withParameters(
+        'Av. Brig. Faria Lima',
+        '1384',
+        'Jardim Paulistano',
+        '01452002',
+        'São Paulo',
+        'SP',
+        'BRA',
+        'apto. 114'
+    );
+
+    //Set billing information for credit card 
+    //This is an example that can be changed to future implementations
+    $creditCard->setBilling()->setAddress()->withParameters(
+        'Av. Brig. Faria Lima',
+        '1384',
+        'Jardim Paulistano',
+        '01452002',
+        'São Paulo',
+        'SP',
+        'BRA',
+        'apto. 114'
+    );
+
+    // Set credit card token
+    $creditCard->setToken($dataPOST['card_token']);
+
+    //Set payment installment
+    list($quantity,$installmentAmount ) = explode('|', $dataPOST['installment']);
     
+    $installmentAmount = number_format($installmentAmount,2,'.','');
+    
+    $creditCard->setInstallment()->withParameters($quantity, $installmentAmount);
+
+    // Set the credit card holder information
+    //This is an example that can be changed to future implementations
+    $creditCard->setHolder()->setBirthdate('01/10/1979');
+
+    $creditCard->setHolder()->setName($dataPOST['card_name']);
+  
+    //This is an example that can be changed to future implementations
+    $creditCard->setHolder()->setPhone()->withParameters(
+        11,
+        56273440
+    );
+
+    //This is an example that can be changed to future implementations
+    $creditCard->setHolder()->setDocument()->withParameters(
+        'CPF',
+        '38883880749'
+    );
+
+    $creditCard->setMode('DEFAULT');
+
+    $result = $creditCard->register(
+        \PagSeguro\Configuration\Configure::getAccountCredentials()
+    );
+
+    var_dump($result);
     }
 
 
--- marketplace/resources/views/checkout.blade.php
+++ marketplace/resources/views/checkout.blade.php
@@ -9,9 +9,14 @@
                 <hr>
             </div>
         </div>
-
-
         <form action="" method="post">
+        <div class="row">
+                <div class="form-group col-md-12">
+                    <label>Person's card name</span></label>
+                    <input type="text" name="card_name"class="form-control">
+                </div>
+            </div>
+
             <div class="row">
                 <div class="form-group col-md-12">
                     <label>Card number <span class="brand"></span></label>
@@ -21,17 +26,14 @@
             </div>
 
             <div class="row">
-
                 <div class="form-group col-md-4">
                     <label>Month expiration</label>
                     <input type="text" name="card_month"class="form-control">
                 </div>
-
                 <div class="form-group col-md-4">
                     <label>Year expiration</label>
                     <input type="text" name="card_year"class="form-control">
                 </div>
-
             </div>
 
             <div class="row">
@@ -44,7 +46,7 @@
                 <div class="col-md-12 installments form-group"></div>
             </div>
 
-            <button class="btn btn-success processCheckout btn-lg">Place order</button>
+            <button class="btn btn-success btn-lg processCheckout">Place order</button>
         </form>
     </div>
 </div>
@@ -54,125 +56,92 @@
 @section('scripts')
 <script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
 <script src="{{asset('assets/js/jquery.ajax.js')}}"></script>
-<script type="text/javascript">
+
+<script>
      const sessionId = '{{session()->get('pagseguro_session_code')}}';
     PagSeguroDirectPayment.setSessionId(sessionId);
-
 </script>
 
 <script>
-    let cardNumber = document.querySelector('input[name=card_number]');
-    let spanBrand = document.querySelector('span.brand');
-    cardNumber.addEventListener('keyup', function(){
-        if(cardNumber.value.length >= 6){
-            PagSeguroDirectPayment.getBrand({
-                cardBin: cardNumber.value.substr(0,6),
-                success: function(res){
-                    let imgFlag = `<img src="https://stc.pagseguro.uol.com.br/public/img/payment-methods-flags/68x30/${res.brand.name}.png">`;
-                    
-                    document.querySelector('input[name=card_brand]').value = res.brand.name;
-
-                    spanBrand.innerHTML = imgFlag;
-                    
-                    getInstallments(40,res.brand.name);
-                },
-                error: function(err){
-                    console.log(err)
-                },
-                complete: function(res){
-                    //console.log(res)
+           let amoutTransaction = '{{$cartItems}}';
+           let cardNumber = document.querySelector('input[name=card_number]');
+           let spanBrand  = document.querySelector('span.brand');
+           cardNumber.addEventListener('keyup', function(){
+                if(cardNumber.value.length >= 6) {
+                    PagSeguroDirectPayment.getBrand({
+                        cardBin: cardNumber.value.substr(0, 6),
+                        success: function(res) {
+                            let imgFlag = `<img src="https://stc.pagseguro.uol.com.br/public/img/payment-methods-flags/68x30/${res.brand.name}.png">`;
+                            spanBrand.innerHTML = imgFlag;
+                            document.querySelector('input[name=card_brand]').value = res.brand.name;
+                            getInstallments(amoutTransaction, res.brand.name);
+                        },
+                        error: function(err) {
+                            console.log(err);
+                        },
+                        complete: function(res) {
+                            //console.log('Complete', res);
+                        }
+                    });
                 }
-            });
-        }
-    });
-
-    let submitButton = document.querySelector('button.processCheckout');
-        submitButton.addEventListener('click',function(event){
-
-            event.preventDefault();
-
-            PagSeguroDirectPayment.createCardToken({
-                cardNumber: document.querySelector('input[name=card_number]').value,
-                brand:      document.querySelector('input[name=card_brand]').value,
-                cvv:        document.querySelector('input[name=card_cvv]').value,
-                expirationMonth: document.querySelector('input[name=card_month]').value,
-                expirationYear:  document.querySelector('input[name=card_year]').value,
-                success: function(res){
-                    processPayment(res.card.token);
-                },
-                error: function(err){
-                    console.log(err);
-                },
-            })
-        });
-
-        function processPayment(token){
-            
-            let data = {
-                card_token: token,
-                hash: PagSeguroDirectPayment.getSenderHash(),
-                installment : document.querySelector('.select_installments').value,
-                _token: '{{csrf_token()}}'
-            };
-            
-
-            $.ajax({
-                type: 'POST',
-                url: '{{route("checkout.proccess")}}',
-                data: data,
-                dataType: 'json',
-                success: function(res){
-                    console.log(res);
-                }
-            });
-        }
-
-
-
-
-
-
-    function getInstallments(amount, brand) {
-    PagSeguroDirectPayment.getInstallments({
-        amount: amount,
-        brand: brand,
-        maxInstallmentNoInterest: 0,
-        success: function(res) {
-            let selectInstallments = drawSelectInstallments(res.installments[brand]);
-            document.querySelector('div.installments').innerHTML = selectInstallments;
-        },
-        error: function(err) {
-            console.log(err);
-        },
-        complete: function(res) {
-
-        },
-    })
-}
-
-    function drawSelectInstallments(installments) {
-		let select = '<label>Installment Options:</label>';
-
-		select += '<select class="form-control select_installments">';
-
-		for(let l of installments) {
-		    select += `<option value="${l.quantity}|${l.installmentAmount}">${l.quantity}x of ${l.installmentAmount} - Total: ${l.totalAmount}</option>`;
-		}
-
-
-		select += '</select>';
-
-		return select;
-	}
-
-
-
-
-
-
-
-</script>
-
-
-
-@endsection
+           });
+           let submitButton = document.querySelector('button.processCheckout');
+           submitButton.addEventListener('click', function(event){
+                event.preventDefault();
+                PagSeguroDirectPayment.createCardToken({
+                    cardNumber: document.querySelector('input[name=card_number]').value,
+                    brand:      document.querySelector('input[name=card_brand]').value,
+                    cvv:        document.querySelector('input[name=card_cvv]').value,
+                    expirationMonth: document.querySelector('input[name=card_month]').value,
+                    expirationYear:  document.querySelector('input[name=card_year]').value,
+                    success: function(res) {
+                        proccessPayment(res.card.token);
+                    }
+                });
+           });
+           function proccessPayment(token)
+           {
+                let data = {
+                    card_token: token,
+                    hash: PagSeguroDirectPayment.getSenderHash(),
+                    installment: document.querySelector('select.select_installments').value,
+                    card_name: document.querySelector('input[name=card_name]').value,
+                    _token: '{{csrf_token()}}'
+                };
+                $.ajax({
+                    type: 'POST',
+                    url: '{{route("checkout.proccess")}}',
+                    data: data,
+                    dataType: 'json',
+                    success: function(res) {
+                        console.log(res);
+                    }
+                });
+           }
+           function getInstallments(amount, brand) {
+               PagSeguroDirectPayment.getInstallments({
+                   amount: amount,
+                   brand: brand,
+                   maxInstallmentNoInterest: 0,
+                   success: function(res) {
+                       let selectInstallments = drawSelectInstallments(res.installments[brand]);
+                       document.querySelector('div.installments').innerHTML = selectInstallments;
+                   },
+                   error: function(err) {
+                         console.log(err);
+                   },
+                   complete: function(res) {
+                   },
+               })
+           }
+           function drawSelectInstallments(installments) {
+               let select = '<label>Opções de Parcelamento:</label>';
+               select += '<select class="form-control select_installments">';
+               for(let l of installments) {
+                   select += `<option value="${l.quantity}|${l.installmentAmount}">${l.quantity}x de ${l.installmentAmount} - Total fica ${l.totalAmount}</option>`;
+               }
+               select += '</select>';
+               return select;
+           }
+    </script>
+@endsection 
