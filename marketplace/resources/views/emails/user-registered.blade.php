18c86cb46cc7708dd7eed2940f47526924629225
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2021-08-11 11:04:03 -0300

    Creating mail service when an user register an account

---
+++ marketplace/resources/views/emails/user-registered.blade.php
@@ -0,0 +1,14 @@
+<h1>{{$user->name}}</h1>
+<h2>thank you for creating your account with us</h2>
+<hr>
+
+<p>
+Your email is: <strong>{{$user->email}}</strong>
+<br>
+Do not share your password with anyone as it grants access to your Marketplace account.
+<br>
+Thanks for shopping with us! We hope to see you again soon.
+</p>
+<hr>
+
+Email submited at {{date('d/m/Y h:i:s')}}.
